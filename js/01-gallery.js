83b393501d4fcd71a47bb452aefb46901d3d6d6d
Artem Riabchych <artemskyart@gmail.com>
2022-12-09 18:50:54 +0200

    add-basicLightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerRef = document.querySelector(".gallery");
+// console.dir(galleryContainerRef);
+// console.log(galleryItems);
+
+const galleryMarkup = creategalleryMarkup(galleryItems);
+
+galleryContainerRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function creategalleryMarkup(items) {
+  return items
+    .map(({ preview, description, original }) => {
+      return `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+        `;
+    })
+    .join("");
+}
+
+galleryContainerRef.addEventListener("click", openOriginalPic);
+
+function openOriginalPic(evt) {
+  evt.preventDefault();
+  //    const isImageEl = evt.target.classlist.contains(".gallery__image");
+  //    if (!isImageEl) { return };
+  console.dir(evt.target.dataset.source);
+  // console.log(evt.target.classlist.contains(".gallery__image"));
+
+  const instance = basicLightbox.create(`
+	<img
+      class="gallery__image"
+      src="${evt.target.dataset.source}"
+      alt="${evt.target.alt}"
+    />
+`);
+
+  instance.show();
+}
+
